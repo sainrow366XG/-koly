554516cb94752316422144d7d38fdb5d5a8e456f
sainrow366XG <55980848+sainrow366XG@users.noreply.github.com>
2019-10-21 19:22:11 +0200

    Add files via upload

---
+++ Anvil/index.php
@@ -0,0 +1,33 @@
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="StyleSheet" type="text/css" href="css.css">
+        <title>Anvil</title>
+    </head>
+    <body>
+        <header>
+            <hgroup>
+                <h1>Anvil</h1>
+            </hgroup>
+        </header>
+        <div>
+            <p><h2>Název enginu:</h2> Počáteční název Scimitar,v roce 2009 se název změnil na Anvil, v roce 2012 na AnvilNext, od roku 2014 je jeho dosavadní název
+                AnvilNext 2.0</p>
+            <p><h2>Autor:</h2>Ubisoft Montreal- Claude Langlais</p>
+        <p><h2>Zajímavosti:</h2> Ubisoft Digital Arts používá aktiva od Anvil znovu vytvořit prostředí, ve kterém jsou živí herci natočený.
+                AnvilNext počínaje Assassin Creed jednoty je schopen generovat struktury pružným a automatickým způsobem, zatímco následující zvláštní 
+                pravidla a šablony designu, což snižuje množství času a ruční práce potřebné pro umělce a designéry vytvářet složité městské prostředí.</p>
+        <p><h2>Vlastnosti:</h2> pre-pečené globálního osvětlení, mapování reflexe, objemovou mlhu, dynamické počasí, dynamický zeleň, vyspělé ovládací mechanika s Fyzicky Based Rendering</p>
+        <p><h2>od do:</h2> od roku 2007 do teď</p>
+        <p><h2>Jazyk:</h2> C++</p>
+        <p><h2>Licence:</h2> Proprietární software</p>
+        <p><h2>Platformy:</h2> Microsoft Windows, PlayStation 3, PlayStation 4, PlayStation Vita, Wii U, Nintendo Switch, Xbox 360, Xbox One</p>
+        <p><h2>Top hry:</h2> Assassin's Creed, Assassin's Creed 2, Assassin's Creed 3, Prince of Persia, Tom Clancy's Rainbow Six Siege, For Honor, Skull & Bones </p>
+        </div>
+
+
+
+    </body>
+</html>
