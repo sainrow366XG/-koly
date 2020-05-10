3a0dfb90dcda2f93282193233b36830f8fa3e20e
sainrow366XG <55980848+sainrow366XG@users.noreply.github.com>
2020-05-10 17:55:05 +0200

    Add files via upload

---
+++ Vizualizace směrování/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="StyleSheet" type="text/css" href="vizualizace.css">
+        <title>Vizualizace site</title>
+    </head>
+    <body>
+        
+        <h1>Vizualizace směrovíní</h1>
+        <form>
+            <p>Zadej směrovač:</p> <input type="text" name="hledat" value="" /> <input type="submit" value="Hledat" />
+            
+        </form>
+        <div>
+            <p><iframe src="tabulka.png" frameborder="0" height="600"
+      width="100%"></iframe></p>
+</div>
+        
+        <div>Tabulka</div>      
+
+<div>
+    <p><iframe src="tabulka.txt" frameborder="0" height="400"
+      width="95%"></iframe></p>
+</div>
+        
+    </body>
+</html>
